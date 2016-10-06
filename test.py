899227fda28fd3e29f2e8315a4e8353a9e7430b3
timipeko <timotej.peter.kovacic@gmail.com>
2016-10-06 12:00:18 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import Adafruit_DHT
+
+def simpletest():
+
+    sensor = Adafruit_DHT.DHT22
+
+    pin = '19'
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
